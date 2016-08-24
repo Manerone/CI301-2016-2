1a2902a12352c6b3e7e562f372a7453bb09e0a70
Matheus Agio Nerone <man13@inf.ufpr.br>
2016-08-24 16:30:37 -0300

    get ips and ports is working

--- Problem01-PortScanner/main.c
+++ Problem01-PortScanner/main.c
@@ -43,11 +43,47 @@ void get_base_ip_and_ip_range(Data* data, char* ip){
 	}
 }
 
+void get_port_range(Data* data, char* ports){
+	uint index = 0;
+	String* port_start = String_new();
+	String* port_end = String_new();
+	// Find start port
+	while(ports[index] != '-' && ports[index] != '\0'){
+		String_add_element(port_start, ports[index]);
+		index++;
+	}
+	data->port_start = String_to_int(port_start);
+	// Find end port if the is one
+	if (ports[index] == '\0')
+		data->port_end = data->port_start;
+	else if(ports[index] == '-'){
+		while(ports[index] != '\0'){
+			index++;
+			String_add_element(port_end, ports[index]);
+		}
+		data->port_end = String_to_int(port_end);
+	}
+}
+
 Data* get_data(int argc, char *argv[]){
 	Data* data = malloc(sizeof(Data));
 	data->base_ip = String_new();
 	get_base_ip_and_ip_range(data, argv[1]);
+	if (argc > 2)
+		get_port_range(data, argv[2]);
+	else{
+		data->port_start = 0;
+		data->port_end = 65535;
+	}
 	return data;
+}
+
+void print_data(Data* data){
+	printf("%s\n", data->base_ip->characters);
+	printf("%d\n", data->ip_start);
+	printf("%d\n", data->ip_end);
+	printf("%d\n", data->port_start);
+	printf("%d\n", data->port_end);
 }
 
 int main(int argc, char *argv[]) {
