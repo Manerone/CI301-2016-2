4814d686eda337d0029cdae4899f1225c6074540
MatheusAN <matheus.agio@hotmail.com>
2016-08-25 17:55:44 -0300

    reworking main

--- Problem01-PortScanner/main.c
+++ Problem01-PortScanner/main.c
@@ -5,40 +5,47 @@
 #include <unistd.h>
 #include "data.c"
 
+void scan(char* ip, int port){
+    int sock;
+    struct sockaddr_in sa;
+    int connection;
+    char buffer[4096];
+    memset(&sa, 0, sizeof(sa));
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr(ip);
+    sa.sin_port = htons(port);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0) {
+      fprintf(stderr, "Cannot create socket");
+      return;
+     }
+     connection = connect(sock, (struct sockaddr*)&sa, sizeof(sa));
+     if (connection >= 0) {
+         memset(buffer, 0, sizeof(buffer));
+         strcpy(buffer, "ls\r\n");
+         connection = write(sock, buffer, strlen(buffer));
+         if(connection < 0) return;
+         bzero(buffer, 4096);
+         connection = read(sock, buffer, 4096);
+         if(connection < 0) return;
+         printf("%s\t%d\t%s\n", ip, port, buffer);
+     }
+     close(sock);
+}
+
 int main(int argc, char *argv[]) {
         Data* data = get_data(argc, argv);
-        int sock;
-        int connection;
+
+
         String* final_part_of_ip;
         char* ip;
-        struct sockaddr_in sa;
-        char buffer[4096];
+
+
         for (uint current_ip = data->ip_start; current_ip <= data->ip_end; ++current_ip) {
             final_part_of_ip = String_int_to_string(current_ip);
             ip = String_concat(data->base_ip, final_part_of_ip);
-            memset(&sa, 0, sizeof(sa));
-            sa.sin_family = AF_INET;
-            sa.sin_addr.s_addr = inet_addr(ip);
-            for (uint port = data->port_start; port <= data->port_end; ++port) {
-                sa.sin_port = htons(port);
-                sock = socket(AF_INET, SOCK_STREAM, 0);
-                connection = connect(sock, (struct sockaddr*)&sa, sizeof(sa));
-                if (connection >= 0) {
-                    memset(buffer, 0, sizeof(buffer));
-                    strcpy(buffer, "garbage\r\n");
-                    connection = write(sock, buffer, strlen(buffer));
-                    if(connection < 0) {
-                        printf("[-]Error writing (Port closed maybe?!)\n");
-                    }
-                    bzero(buffer, 4096);
-                    connection = read(sock, buffer, 4096);
-                    if(connection < 0) {
-                        printf("[-]Error reading (Port closed maybe?!)\n");
-                    }
-                    printf("%s\t%d\t%s\n", ip, port, buffer);
-                }
-                close(sock);
-            }
+            for (uint port = data->port_start; port <= data->port_end; ++port)
+                scan(ip, port);
         }
         return 0;
 }
